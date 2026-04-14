ca14a5e37ef9f498ee1e6381810b917c49f1f925
daliH-4cls <daliah2rnndz10@gmail.com>
2026-04-14 15:44:18 +0000

    Completed I think

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 console.log("Script started")
 
-// TODO: Test working with objects here
+// DONE: Test working with objects here
+let person = {name: "Scooby", age: 80};
+console.log(person.name);
+person.name = "Doo";
+console.log(person.name);
+person.snn = "560-08-7900";
+console.log(person);
 
-
-
-function processForm() {
+function processForm(event) {
     console.log("click");
-    // TODO: prevent this function from reloading the page when the form is submitted
+    // DONE: prevent this function from reloading the page when the form is submitted
+    event.preventDefault();
 
-    // TODO: Create a newUser object that has all the user's info from the form
+    // DONE: Create a newUser object that has all the user's info from the form
+    let newUser = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        birthdate: document.getElementById("birthdate").value,
+        favColor: document.getElementById("favcolor").value
+    }
 
     // TODO: Call the addUser function and pass the newUser object as a parameter
+    addUser(newUser);
 }
 
 function addUser(user) {
     // Create a div to hold the user's info
     let div = document.createElement("div");
-    // TODO: Set the background color of the div to the user's favorite color
+    // DONE: Set the background color of the div to the user's favorite color
+    div.style.backgroundColor = user.favColor;
 
     // Create a heading for the user's name
     let name = document.createElement("h2");
-    // TODO: Set the text using the object data
+    // DONE: Set the text using the object data
+    name.innerText = user.name;
     div.appendChild(name); // Add the heading to the div
 
     // Create a paragraph for the user's email
     let email = document.createElement("p");
-    // TODO: Set the text using the object data
+    // DONE: Set the text using the object data
+    email.innerText = user.email;
     div.appendChild(email); // Add the paragraph to the div
     
     // Create a paragrapn for the user's birthdate
     let birthdate = document.createElement("p");
-    // TODO: Set the text using the object data
+    // DONE: Set the text using the object data
+    birthdate.innerText = user.birthdate;
     div.appendChild(birthdate); // Add the paragraph to the div
 
     document.body.appendChild(div); // Add the div to the page
